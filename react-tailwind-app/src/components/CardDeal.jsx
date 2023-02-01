df2423d0451ceecea3b71a45f9e423415a600389
dvshannon <dvshannon96@gmail.com>
2023-01-31 21:00:12 -0500

    cards added

--- react-tailwind-app/src/components/CardDeal.jsx
+++ react-tailwind-app/src/components/CardDeal.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { card } from '../assets'
+import styles, { layout } from '../style'
+import Button from './Button'
 
-const CardDeal = () => {
-  return (
-    <div>CardDeal</div>
-  )
-}
+const CardDeal  = () => (
+   <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>Find a better card deal <br className='sm:block hidden'/> in a few easy steps</h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam quibusdam alias dolorem perferendis, dolor in iusto totam, officiis eius sed repellendus quaerat. Velit, autem doloribus magnam nisi repellendus quos quam.</p>
+      <Button styles='mt-10'/>
+    </div>
+
+    <div className={layout.sectionImg}>
+      <img src={card} alt="card" className='w-[100%] h-auto' />
+    </div>
+   </section>
+)
 
 export default CardDeal
