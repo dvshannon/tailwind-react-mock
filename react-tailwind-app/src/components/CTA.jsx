04a91efede68e810097ee8899d0a9b57d31bdbbb
dvshannon <dvshannon96@gmail.com>
2023-02-06 20:57:37 -0500

    added footer updated tailwind config to include max-width media queries

--- react-tailwind-app/src/components/CTA.jsx
+++ react-tailwind-app/src/components/CTA.jsx
@@ -1,13 +1,14 @@
 import styles from '../style'
-import Button from './button'
+import Button from './Button'
 
 const CTA = () => (
-    <section className={`${styles.flexCenter} ${styles.marginY}`}>
-      <div>
-        <h2>Try Our Service Today!</h2>
-        <p>Everything you need to accept card payments and grow your business anywhere on the planet.</p>
+    <section className={`${styles.flexCenter} ${styles.padding} ${styles.marginY} -sm:flex-col flex-row bg-black-gradient-2 rounded-[20px] box-shadow`}>
+      <div className='flex-1 flex flex-col'>
+        <h2 className={styles.heading2}>Try our service today!</h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Everything you need to accept card payments and grow your business anywhere on the planet.</p>
       </div>
-      <div>
+
+      <div className={`${styles.flexCenter} sm:ml-10 ml-0 sm:mt-0 mt-10`}>
         <Button />
       </div>
     </section>
--- react-tailwind-app/tailwind.config.cjs
+++ react-tailwind-app/tailwind.config.cjs
@@ -22,6 +22,15 @@ module.exports = {
       md: "1060px",
       lg: "1200px",
       xl: "1700px",
+
+      // max-width queries
+
+      '-xs': "480px",
+      '-ss': "620px",
+      '-sm': "768px",
+      '-md': "1060px",
+      '-lg': "1200px",
+      '-xl': "1700px",
     },
   },
   plugins: [],
